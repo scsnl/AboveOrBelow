adfb118b42806edf8e42d8c2f12a7e8b7a7cb1d4
Sachin Allums <138811459+sachinallums@users.noreply.github.com>
2023-08-28 10:54:43 -0700

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/sachinallums/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/sachinallums/Downloads/scsnl_aboveorbelow"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/sachinallums/Downloads/scsnl_aboveorbelow/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8zM2ZjYTAyNDUxZWYzMTRmYzY1NTYzNGIxMTZhN2I2ZDExNDRiOWY3Lw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/sachinallums/Downloads/scsnl_aboveorbelow/.dart_tool/package_config.json"
